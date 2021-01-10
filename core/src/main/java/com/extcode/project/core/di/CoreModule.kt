29440a37959ee176934a02ce326dc8863befaff0
Adithya-13 <aditputrafirmansyah@gmail.com>
2021-01-11 05:37:32 +0700

    encryption database

--- core/src/main/java/com/extcode/project/core/di/CoreModule.kt
+++ core/src/main/java/com/extcode/project/core/di/CoreModule.kt
@@ -8,6 +8,8 @@ import com.extcode.project.core.data.source.remote.RemoteDataSource
 import com.extcode.project.core.data.source.remote.network.ApiService
 import com.extcode.project.core.domain.repository.IMovieAppRepository
 import com.extcode.project.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,10 +21,14 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("codext".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java, "movie.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
