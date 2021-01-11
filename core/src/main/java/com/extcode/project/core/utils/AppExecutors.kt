166ed846dda6ed340c5e9e10587a2c37a22f52fb
Adithya-13 <aditputrafirmansyah@gmail.com>
2021-01-12 00:00:09 +0700

    remove unused files

--- core/src/main/java/com/extcode/project/core/utils/AppExecutors.kt
+++ core/src/main/java/com/extcode/project/core/utils/AppExecutors.kt
@@ -1,41 +1,8 @@
 package com.extcode.project.core.utils
 
-import android.os.Handler
-import android.os.Looper
 import androidx.annotation.VisibleForTesting
-
 import java.util.concurrent.Executor
-import java.util.concurrent.Executors
 
-class AppExecutors @VisibleForTesting constructor(
-    private val diskIO: Executor,
-    private val networkIO: Executor,
-    private val mainThread: Executor
-) {
-
-    companion object {
-        private const val THREAD_COUNT = 3
-    }
-
-    constructor() : this(
-        Executors.newSingleThreadExecutor(),
-        Executors.newFixedThreadPool(THREAD_COUNT),
-        MainThreadExecutor()
-    )
-
+class AppExecutors @VisibleForTesting constructor(private val diskIO: Executor) {
     fun diskIO(): Executor = diskIO
-
-    //for network
-//    fun networkIO(): Executor = networkIO
-
-    //for mainThread
-//    fun mainThread(): Executor = mainThread
-
-    private class MainThreadExecutor : Executor {
-        private val mainThreadHandler = Handler(Looper.getMainLooper())
-
-        override fun execute(command: Runnable) {
-            mainThreadHandler.post(command)
-        }
-    }
 }
