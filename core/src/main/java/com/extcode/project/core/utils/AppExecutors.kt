ff0c9de08f5afaa28ae9d82df7fb210c68abbb05
Adithya-13 <aditputrafirmansyah@gmail.com>
2021-01-12 00:09:58 +0700

    fix AppExecutors.kt

--- core/src/main/java/com/extcode/project/core/utils/AppExecutors.kt
+++ core/src/main/java/com/extcode/project/core/utils/AppExecutors.kt
@@ -1,8 +1,41 @@
 package com.extcode.project.core.utils
 
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.VisibleForTesting
-import java.util.concurrent.Executor
 
-class AppExecutors @VisibleForTesting constructor(private val diskIO: Executor) {
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class AppExecutors @VisibleForTesting constructor(
+    private val diskIO: Executor,
+    private val networkIO: Executor,
+    private val mainThread: Executor
+) {
+
+    companion object {
+        private const val THREAD_COUNT = 3
+    }
+
+    constructor() : this(
+        Executors.newSingleThreadExecutor(),
+        Executors.newFixedThreadPool(THREAD_COUNT),
+        MainThreadExecutor()
+    )
+
     fun diskIO(): Executor = diskIO
+
+    //for network
+//    fun networkIO(): Executor = networkIO
+
+    //for mainThread
+//    fun mainThread(): Executor = mainThread
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
 }
