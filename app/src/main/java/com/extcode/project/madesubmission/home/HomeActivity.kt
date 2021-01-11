3435486ad760660b2ca554a21058c3f7f454143c
Adithya-13 <aditputrafirmansyah@gmail.com>
2021-01-11 08:42:03 +0700

    obfuscation proguard

--- app/src/main/java/com/extcode/project/madesubmission/home/HomeActivity.kt
+++ app/src/main/java/com/extcode/project/madesubmission/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.extcode.project.madesubmission.home
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
--- core/src/main/java/com/extcode/project/core/di/CoreModule.kt
+++ core/src/main/java/com/extcode/project/core/di/CoreModule.kt
@@ -10,6 +10,7 @@ import com.extcode.project.core.domain.repository.IMovieAppRepository
 import com.extcode.project.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -34,10 +35,15 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
